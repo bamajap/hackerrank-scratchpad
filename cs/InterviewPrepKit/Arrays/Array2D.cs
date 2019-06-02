cb8cb7320b1e24b8bad1b208ecfb31d325cc1b2b
Jason Payne <22374001+bamajap@users.noreply.github.com>
2019-06-02 10:49:07 -0500

    Added C# solutions for a couple of problems already completed.
    Setup structure for future submissions.

---
+++ cs/InterviewPrepKit/Arrays/Array2D.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/2d-array/problem
+    /// </summary>
+    internal static class Array2D
+    {
+        internal static void Run()
+        {
+            var tests = new List<int[][]>
+            {
+                new[]
+                {
+                    new[] {-9, -9, -9, 1, 1, 1},
+                    new[] {0, -9, 0, 4, 3, 2},
+                    new[] {-9, -9, -9, 1, 2, 3},
+                    new[] {0, 0, 8, 6, 6, 0},
+                    new[] {0, 0, 0, 0, -2, 0},
+                    new[] {0, 0, 1, 2, 4, 0},
+                },
+                new[]
+                {
+                    new[] {1, 1, 1, 0, 0, 0},
+                    new[] {0, 1, 0, 0, 0, 0},
+                    new[] {1, 1, 1, 0, 0, 0},
+                    new[] {0, 0, 2, 4, 4, 0},
+                    new[] {0, 0, 0, 2, 0, 0},
+                    new[] {0, 0, 1, 2, 4, 0},
+                },
+            };
+            foreach (var testArray in tests)
+            {
+                Console.WriteLine(hourglassSum(testArray));                
+            }
+        }
+
+        // Complete the hourglassSum function below.
+        static int hourglassSum(int[][] arr)
+        {
+            int xLen = 4, yLen = 4, max = 0, sum;
+            bool first = true;
+            for (int x = 0; x < xLen; x++)
+            {
+                for (int y = 0; y < yLen; y++)
+                {
+                    sum =   arr[x][y] + arr[x][y + 1] + arr[x][y + 2] +
+                            arr[x + 1][y + 1] +
+                            arr[x + 2][y] + arr[x + 2][y + 1] + arr[x + 2][y + 2];
+                    if (first)
+                    {
+                        first = false;
+                        max = sum;
+                        continue;
+                    }
+                    if (sum > max) max = sum;
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ cs/InterviewPrepKit/Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Array2D.Run();
+            LeftRotation.Run();
+
+            Console.WriteLine(Environment.NewLine + "Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+    /// </summary>
+    internal static class LeftRotation
+    {
+        internal static void Run()
+        {
+            Console.WriteLine(string.Join(", ", rotLeft(new[] {1, 2, 3, 4, 5}, 2)));
+            Console.WriteLine(string.Join(", ", rotLeft(new[] {1, 2, 3, 4, 5}, 4)));
+        }
+
+        // Complete the rotLeft function below.
+        private static int[] rotLeft(int[] a, int d)
+        {
+            int aLen = a.Length;
+            if (d == aLen) return a;
+
+            var result = new int[aLen];
+            for (int i = 0; i < aLen; i++)
+            {
+                result[i] = a[(d + i)%aLen];
+            }
+            return result;
+        }
+    }
+}
